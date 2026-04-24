7b865bed902718367500aaa5e8dee7c3206d883a
its-me-abhiram <abhiram143sag@gmail.com>
2026-04-24 09:52:47 +0530

    Initial commit

--- skillbridge/settings.py
+++ skillbridge/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure--60!&1-2x(j9(f%-60pk-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = ['*'] if DEBUG else config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
